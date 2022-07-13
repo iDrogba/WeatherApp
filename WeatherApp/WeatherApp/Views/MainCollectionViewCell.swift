d4bcb01e5322a0eaaa5e98e0f0477d68559825a0
김상현 <kshy1019@naver.com>
2022-07-13 17:29:20 +0900

    [Feat] #12 add label -3
    서핑 컨디션 라벨 추가

--- WeatherApp/WeatherApp/Views/MainCollectionViewCell.swift
+++ WeatherApp/WeatherApp/Views/MainCollectionViewCell.swift
@@ -10,6 +10,16 @@ import UIKit
 class MainCollectionViewCell: UICollectionViewCell {
     static let reuseIdentifier = "MainCollectionViewCell"
     
+    private let surfConditionLabel: UILabel = {
+        let surfConditionLabel = UILabel(frame: CGRect(origin: .zero, size: .zero))
+        surfConditionLabel.translatesAutoresizingMaskIntoConstraints = false
+        surfConditionLabel.text = "서핑하기 좋아요."
+        surfConditionLabel.font = .systemFont(ofSize: 12, weight: .semibold)
+        surfConditionLabel.textColor = .white
+        
+        return surfConditionLabel
+    }()
+    
     private let weatherLabel: UILabel = {
         let weatherLabel = UILabel(frame: CGRect(origin: .zero, size: .zero))
         weatherLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -24,7 +34,7 @@ class MainCollectionViewCell: UICollectionViewCell {
         let minTemperatureLabel = UILabel(frame: CGRect(origin: .zero, size: .zero))
         minTemperatureLabel.translatesAutoresizingMaskIntoConstraints = false
         minTemperatureLabel.text = "최저:17°"
-        minTemperatureLabel.font = .systemFont(ofSize: 12, weight: .semibold)
+        minTemperatureLabel.font = .systemFont(ofSize: 12, weight: .regular)
         minTemperatureLabel.textColor = .white
         
         return minTemperatureLabel
@@ -34,7 +44,7 @@ class MainCollectionViewCell: UICollectionViewCell {
         let maxTemperatureLabel = UILabel(frame: CGRect(origin: .zero, size: .zero))
         maxTemperatureLabel.translatesAutoresizingMaskIntoConstraints = false
         maxTemperatureLabel.text = "최고:28°"
-        maxTemperatureLabel.font = .systemFont(ofSize: 12, weight: .semibold)
+        maxTemperatureLabel.font = .systemFont(ofSize: 12, weight: .regular)
         maxTemperatureLabel.textColor = .white
         
         return maxTemperatureLabel
@@ -75,6 +85,7 @@ class MainCollectionViewCell: UICollectionViewCell {
         self.addSubview(minTemperatureLabel)
         self.addSubview(maxTemperatureLabel)
         self.addSubview(weatherLabel)
+        self.addSubview(surfConditionLabel)
         self.backgroundColor = .gray
         self.clipsToBounds = true
     }
@@ -119,8 +130,12 @@ class MainCollectionViewCell: UICollectionViewCell {
             minTemperatureLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10)
         ]
         let weatherLabelConstraints = [
-            weatherLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
-            weatherLabel.centerYAnchor.constraint(equalTo: maxTemperatureLabel.centerYAnchor, constant: 0)
+            weatherLabel.trailingAnchor.constraint(equalTo: minTemperatureLabel.trailingAnchor, constant: 0),
+            weatherLabel.bottomAnchor.constraint(equalTo: maxTemperatureLabel.topAnchor, constant: 0)
+        ]
+        let surfConditionLabelConstraints = [
+            surfConditionLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            surfConditionLabel.centerYAnchor.constraint(equalTo: maxTemperatureLabel.centerYAnchor, constant: 0)
         ]
         NSLayoutConstraint.activate(imageViewConstraints)
         NSLayoutConstraint.activate(regionLabelConstraints)
@@ -128,5 +143,6 @@ class MainCollectionViewCell: UICollectionViewCell {
         NSLayoutConstraint.activate(maxTemperatureLabelConstraints)
         NSLayoutConstraint.activate(minTemperatureLabelConstraints)
         NSLayoutConstraint.activate(weatherLabelConstraints)
+        NSLayoutConstraint.activate(surfConditionLabelConstraints)
     }
 }

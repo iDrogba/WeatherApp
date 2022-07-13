9fafb3609a014e3e766bfd3551a2c01255f3749e
김상현 <kshy1019@naver.com>
2022-07-13 16:54:16 +0900

    [Feat] #12 add label
    지역 라벨, 현재 온도 라벨

--- WeatherApp/WeatherApp/Views/MainCollectionViewCell.swift
+++ WeatherApp/WeatherApp/Views/MainCollectionViewCell.swift
@@ -10,8 +10,28 @@ import UIKit
 class MainCollectionViewCell: UICollectionViewCell {
     static let reuseIdentifier = "MainCollectionViewCell"
     
+    private let temperatuerLabel: UILabel = {
+        let temperatuerLabel = UILabel(frame: CGRect(origin: .zero, size: .zero))
+        temperatuerLabel.translatesAutoresizingMaskIntoConstraints = false
+        temperatuerLabel.text = "25°"
+        temperatuerLabel.font = .systemFont(ofSize: 48, weight: .medium)
+        temperatuerLabel.textColor = .white
+        
+        return temperatuerLabel
+    }()
+    
+    private let regionLabel: UILabel = {
+        let regionLabel = UILabel(frame: CGRect(origin: .zero, size: .zero))
+        regionLabel.translatesAutoresizingMaskIntoConstraints = false
+        regionLabel.text = "포항시"
+        regionLabel.font = .systemFont(ofSize: 24, weight: .semibold)
+        regionLabel.textColor = .white
+        
+        return regionLabel
+    }()
+    
     private let imageView: UIImageView = {
-        let imageView = UIImageView()
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
         return imageView
@@ -20,7 +40,8 @@ class MainCollectionViewCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.addSubview(imageView)
-        self.imageView.frame = bounds
+        self.addSubview(regionLabel)
+        self.addSubview(temperatuerLabel)
         self.backgroundColor = .gray
         self.clipsToBounds = true
     }
@@ -29,8 +50,35 @@ class MainCollectionViewCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        DispatchQueue.main.async {
+            self.setConstraints()
+        }
+    }
+    
     func setUI() {
         self.layer.cornerRadius = 4
-        imageView.image = UIImage(named: "서핑2.png")
+        imageView.image = UIImage(named: "surf2.png")
+    }
+    
+    func setConstraints() {
+        let imageViewConstraints = [
+            imageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 10),
+            imageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10),
+            imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor),
+            imageView.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+        ]
+        let regionLabelConstraints = [
+            regionLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            regionLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 15)
+        ]
+        let temperatureLabelConstraints = [
+            temperatuerLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+            temperatuerLabel.topAnchor.constraint(equalTo: regionLabel.topAnchor, constant: 0),
+        ]
+        NSLayoutConstraint.activate(imageViewConstraints)
+        NSLayoutConstraint.activate(regionLabelConstraints)
+        NSLayoutConstraint.activate(temperatureLabelConstraints)
     }
 }

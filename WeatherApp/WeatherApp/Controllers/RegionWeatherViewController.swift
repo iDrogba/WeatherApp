26cb741557a9416e3acd88b71bb1f6266ec7f4c4
lena <lenaminn@gmail.com>
2022-07-20 17:16:12 +0900

    [FEAT] #17 Pull down to dismiss 기능 구현

--- WeatherApp/WeatherApp/Controllers/RegionWeatherViewController.swift
+++ WeatherApp/WeatherApp/Controllers/RegionWeatherViewController.swift
@@ -18,6 +18,8 @@ class RegionWeatherViewController: UIViewController {
             view.addSubview($0)
         }
         
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handleDismiss)))
+        
         weekWeatherTableView.delegate = self
         weekWeatherTableView.dataSource = self
         
@@ -224,6 +226,27 @@ class RegionWeatherViewController: UIViewController {
         maxTemperatureLabel.text = pastTMXModel.TMP + "°"
         descriptionLabel.text = descriptionLabelText
         waveHeightLabel.text = model.WAV + "m"
+    }
+    
+    var viewTranslation = CGPoint(x: 0, y:0)
+    @objc func handleDismiss(sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .changed:
+            viewTranslation = sender.translation(in: view)
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                self.view.transform = CGAffineTransform(translationX: 0, y: self.viewTranslation.y)
+            })
+        case .ended:
+            if viewTranslation.y < 200 {
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                    self.view.transform = .identity
+                })
+            } else {
+                dismiss(animated: true, completion: nil)
+            }
+        default:
+            break
+        }
     }
     
     private func configureConstraints() {
